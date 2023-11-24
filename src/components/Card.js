f0a20fc1c333c53444698d398e29848bd50cfe3d
Nikhilraju7 <nikhilrajupalatty777@gmail.com>
2023-11-24 15:50:41 +0530

    card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,22 @@
-import schoolImg from "../../public/school.jpg";
+import Image from 'next/image';
+import schoolImg from '../../public/school.jpg'
 
 const Card = () => {
   return (
-    <div>
-      <img
+    <div className="h-[350px] w-[280px] bg-slate-100 rounded-2xl p-3 flex flex-col gap-2 drop-shadow-lg">
+      <Image
         src={schoolImg}
         alt="school"
+        className='rounded-xl '
       />
-      <h2>Card Title</h2>
-      <p>Card Description</p>
-      <button>Click</button>
+      <div className='flex flex-col gap-3'>
+      <h2 className='font-bold text-lg text-slate-500'>Card Title</h2>
+      <p className='text-sm font-light'>this is the description of the expences for a specific type</p>
+      <div className='flex justify-center align-middle mt-3'>
+      <p className='text-3xl text-green-500 font-bold'>5000</p>
+      </div>
+      </div>
+      
     </div>
   );
 };
