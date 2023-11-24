9dc94ca665572fd77b754f91d9fb6d804d839b9e
Anvin.C.Shaju <anvincsg@gmail.com>
2023-11-24 12:03:27 +0530

    Refactor sidebar component and add navigation menu

--- src/app/(data)/layout.js
+++ src/app/(data)/layout.js
@@ -1,7 +1,13 @@
+"use client";
+
+import Sidebar from "@/components/Sidebar";
+
 export default function SideLayout({ children }) {
   return (
     <div className="flex">
-      <div className="flex-2 w-[30%]">Sidebar</div>
+      <div className="w-[15%] bg-red-400 h-full">
+        <Sidebar />
+      </div>
       <main>{children}</main>
     </div>
   );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,29 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <NavigationMenu className="bg-green-300 h-screen w-[100%]">
+      <NavigationMenuList className="flex flex-col justify-start items-start">
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Item Two</NavigationMenuTrigger>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Item Three</NavigationMenuTrigger>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
 };
 export default Sidebar;
