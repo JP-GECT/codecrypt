fa6dc4ba8d0a33a68229ce60569c7a43e9c85c80
JP-GECT <josephpauly3295@gmail.com>
2023-11-24 22:35:25 +0530

    not included 3d

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,6 +1,6 @@
-const VideoCard = ({ embedId, description }) => {
+const VideoCard = ({ embedId, description,heading }) => {
   return (
-    <div className="flex items-center justify-center lg:justify-start">
+    <div className="flex  justify-center lg:justify-start bg-slate-100 drop-shadow-sm p-3 rounded-2xl hover:bg-slate-200">
       <div className="lg:w-1/2 aspect-w-16 aspect-h-9">
         <iframe
           width="800" // Adjusted width
@@ -9,10 +9,11 @@ const VideoCard = ({ embedId, description }) => {
           title="YouTube video player"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowFullScreen
-          className="w-full h-full rounded-lg min-h-[300px]"
+          className="w-full h-full rounded-2xl min-h-[300px]"
         ></iframe>
       </div>
-      <div className="lg:w-1/2 p-4">
+      <div className="lg:w-1/2 p-4 flex flex-col  gap-3 ">
+        <h2 className="text-[30px] font-bold justify-center items-start">{heading}</h2>
         <p className="text-gray-700">{description}</p>
       </div>
     </div>
